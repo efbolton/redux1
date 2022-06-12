89f4c175bf2d15624dbbfd39ed5b6a408ff03abf
efbolton <EFBOLTON@HOTMAIL.COM>
2022-06-12 08:57:29 -0400

    20220612
    20220612

---
+++ src/components/ChangeColor.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { changeColor } from "../features/theme";
+
+function ChangeColor() {
+  const [color, setColor] = useState("red");
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter color"
+        onChange={(event) => {
+          setColor(event.target.value);
+        }}
+      />
+      <button onClick={() => dispatch(changeColor(color))}>CHANGE COLOR</button>
+    </div>
+  );
+}
+
+export default ChangeColor;
